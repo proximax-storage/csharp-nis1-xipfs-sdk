a233b1330f936ff502e7094afa674cbb6d6e291d
jcapio <joseph.capio@proximax.io>
2018-05-22 22:15:19 +0800

    Generic Rest Request

---
+++ src/IO.XPX.Standard/Facades/BaseRequest.cs
@@ -0,0 +1,47 @@
+﻿using IO.XPX.Standard.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace IO.XPX.Standard.Facades
+{
+    public static class BaseRequest
+    {
+        private static GenericResponse<T> CreateAPIRequest<T>(string url , string method) where T : class
+        {
+            try
+            {
+                Stream dataStream;
+                WebRequest request = WebRequest.Create(url);
+                request.ContentType = "application/json";
+                request.Method = "GET";
+
+                WebResponse response = request.GetResponse();
+
+                var status = ((HttpWebResponse)response).StatusDescription;
+
+                dataStream = response.GetResponseStream();
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                var streamResponse = reader.ReadToEnd();
+
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+                return new GenericResponse<T>() { Result = JsonConvert.DeserializeObject<T>(streamResponse) };
+            }
+            catch (Exception ex)
+            {
+                return new GenericResponse<T>() { ErrorMessage = ex.Message };
+            }
+        }
+
+        public static GenericResponse<T> GET<T>(string url) where T : class => CreateAPIRequest<T>(url, "GET");
+        public static GenericResponse<T> POST<T>(string url) where T : class => CreateAPIRequest<T>(url, "POST");
+    }
+}
---
+++ src/IO.XPX.Standard/Models/GenericResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IO.XPX.Standard.Models
+{
+    public class GenericResponse<T> where T : class
+    {
+        public T Result { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
