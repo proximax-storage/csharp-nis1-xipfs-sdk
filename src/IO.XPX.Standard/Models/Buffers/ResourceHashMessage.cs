f19cfe6fde13cd178b724c9788ba0db914ad20ba
Richie Golez <ricardogolez@gmail.com>
2018-05-23 22:05:51 +0800

    [+] Initial Classes required for download

---
+++ src/IO.XPX.Standard/Models/Buffers/ResourceHashMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO; // for bytebuffer. need to research more on this one
+
+namespace IO.XPX.Standard.Models.Buffers
+{
+    public sealed class ResourceHashMessage
+    {
+        public static ResourceHashMessage getRootAsResourceHashMessage(byte _bb) { return getRootAsResourceHashMessage(_bb, new ResourceHashMessage()); } // need to get bytebuffer equivalent for c# hmmmm...
+        public static ResourceHashMessage getRootAsResourceHashMessage(byte _bb, ResourceHashMessage obj) { return new ResourceHashMessage(); }
+
+        public void __init(int _i, byte _bb)
+        {
+            //gotta do something with this bytebuffers hahaha
+            int bb_pos = _i; // something like bytebuffer position at start _i
+            byte bb = _bb;
+        }
+
+        public ResourceHashMessage __assign(int _i, byte _bb)
+        {
+            __init(_i, _bb);
+
+            return this;
+        }
+
+        public string digest()
+        {
+            return string.Empty;
+        }
+    }
+}
